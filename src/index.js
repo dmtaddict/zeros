bb9c85d0904668ec08e64e70f3c5baf7b61990b0
dmtaddict <dmtaddict@gmail.com>
2019-10-02 10:04:57 +0700

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,47 +1,60 @@
 module.exports = function zeros(expression) {
-    let numbers= expression.split('*');
+    let numbers= expression.match(/\d+/g).map(Number);
     let result2 = 0;
     let result5 = 0;
+    let j = 0;
     numbers.forEach(function(number){
-        if(number.indexOf('!!') < 0)  {//если просто факториал
-            for (let i=1; i<=number;i++){
-                if(i%2===0) {
-                    result2++;
-                }
-                if(i%5===0) {
-                    result5++;
-                }
-                if(i%25===0){
-                    result5++;
-                }
+        if(expression.charAt(expression.indexOf(number, j)+number.toString().length+1)!=='!'||
+            expression.length === expression.indexOf(number, j)+number.toString().length + 1) {//если просто факториал
+            let dec2 = number;
+            while (dec2 >= 2){
+                dec2 = Math.floor(dec2/2);
+                result2 += dec2;
+            }
+            let dec5 = number;
+            while (dec5 >= 5){
+                dec5 = Math.floor(dec5/5);
+                result5 += dec5;
             }
         }
-        else if(number.indexOf('!!')>0&&number.slice(0, number.indexOf('!!'))%2===0 ) {//если двойной факториал и четное
-            for (let i=2; i<=number;i+=2){
-                if(i%2===0) {
-                    result2++;
-                }
-                if(i%5===0) {
-                    result5++;
-                }
-                if(i%25===0){
-                    result5++;
-                }
+        else if(expression.charAt(expression.indexOf(number, j)+number.toString().length+1) ==='!'&&
+            (number % 2) === 0) {//если двойной факториал и четное
+            let dec2 = number;
+            while (dec2 >= 2){
+                dec2 = Math.floor(dec2/2);
+                result2 += dec2;
+            }
+            let dec5 = number/2;
+            while (dec5 >= 5){
+                dec5 = Math.floor(dec5/5);
+                result5 += dec5;
             }
         }
-        else {//если двойной факториал и нечетное
-            for (let i=1; i<=number;i+=2){
-
-                if(i%5===0) {
-                    result5++;
-                }
-                if(i%25===0){
-                    result5++;
-                }
+        else if(expression.charAt(expression.indexOf(number, j)+number.toString().length+1) ==='!'&&
+            (number % 2) !== 0){//если двойной факториал и нечетное
+            let resultn1 = 0;
+            let resultn2 = 0;
+            let decn1 = number;
+            while (decn1 >= 5){
+                decn1 = Math.floor(decn1/5);
+                resultn1+=decn1;
             }
+            let decn2 = (number - 1)/2;
+            while (decn2 >= 5){
+                decn2 = Math.floor(decn2/5);
+                resultn2+=decn2;
+            }
+            result5 = result5 + (resultn1 - resultn2);
         }
-
+        j++;
+        while(expression.charAt(j) !=='*'){
+            if (expression.length <= j) {
+                break;
+            }
+            j++;
+        }
     });
-    return (result2<=result5)?result2:result5;
+     return (result2<=result5)?result2:result5;
+
 };
 
